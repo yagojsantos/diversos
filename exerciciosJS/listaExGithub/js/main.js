237fbc1f487d6ec2748392b8f0c106653a982599
Yago de Jesus Santos <yago_jesus@outlook.com.br>
2023-01-27 12:10:11 -0300

    Finalizado o ex 4

--- exerciciosJS/listaExGithub/js/main.js
+++ exerciciosJS/listaExGithub/js/main.js
@@ -1,9 +1,20 @@
  
 function exercicioDois(){
-    return alert("Olá Mundo");
+    return document.alert("Olá Mundo");
 }
 
 function exercicioTres(){
     let result = 2+2;
     return alert(`O resultado de 2 + 2 é: ${result}`);
+}
+
+function exercicioQuatro(){
+    const campo = document.querySelector("#nomeCompleto");
+    const botao = document.querySelector("#btnExibirNome");
+
+    botao.addEventListener("click",function(){
+        const nome = campo.value;   
+        return alert(`O Nome Completo é: ${nome}`);
+    
+    })
 }
